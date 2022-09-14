65e989b1d407d5c21796052ae639344643f4f5a1
batwang <batwang@tencent.com>
2022-09-14 12:10:03 +0800

    update code

---
+++ scripts/2-4_data_preprocess.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+cd "$(dirname $0)/.."
+echo "$(dirname $0)/.."
+source scripts/env.sh
+
+# STEP2: extract_images_from_video_data_src
+python "$DFL_MAIN" videoed extract-video \
+  --input-file "$WORKSPACE/data_src.*" \
+  --output-dir "$WORKSPACE/data_src"
+
+# STEP3: extract_images_from_video_data_dst
+python "$DFL_MAIN" videoed extract-video \
+  --input-file "$WORKSPACE/data_dst.*" \
+  --output-dir "$WORKSPACE/data_dst"
+
+# STEP4:
+#   1) data_src_extract_faces_S3FD
+python "$DFL_MAIN" extract \
+  --input-dir "$WORKSPACE/data_src" \
+  --output-dir "$WORKSPACE/data_src/aligned" \
+  --detector s3fd
+#   2) data_src_sort
+python "$DFL_MAIN" sort --input-dir "$WORKSPACE/data_src/aligned"
+
+# STEP5:
+#   1) data_dst_extract_faces_S3FD
+python "$DFL_MAIN" extract \
+  --input-dir "$WORKSPACE/data_dst" \
+  --output-dir "$WORKSPACE/data_dst/aligned" \
+  --detector s3fd
+#   2) data_dst_sort
+python "$DFL_MAIN" sort --input-dir "$WORKSPACE/data_dst/aligned"
+
+ehco "preprocess is done !!!"
--- scripts/8_merged_to_mp4.sh
+++ scripts/8_merged_to_mp4.sh
@@ -2,6 +2,9 @@
 cd "$(dirname $0)/.."
 source scripts/env.sh
 
+# 解决mac无法确认开发者的问题
+sudo spctl --master-disable
+
 python "$DFL_MAIN" videoed video-from-sequence \
   --input-dir "$WORKSPACE/data_dst/merged" \
   --output-file "$WORKSPACE/result.mp4" \
